fe6f5f65941b77c41dfbcdcecf36d3e3d0723794
Bilal <ibilalahmad13@gmail.com>
2022-07-30 02:15:28 +0500

    tabbar continue

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bgImageView: UIImageView!
+    
+    @IBOutlet weak var ordersView: UIView!
+    @IBOutlet weak var worksView: UIView!
+    @IBOutlet weak var aboutUsView: UIView!
+    @IBOutlet weak var contactUsView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.bgImageView.layer.cornerRadius = self.bgImageView.frame.width/6.0
+        //bgImageView.round(corners: [.bottomLeft, .bottomRight], cornerRadius: 20)
+        //bgImageView.roundCorners(with: [.layerMinXMaxYCorner], radius: 20)
+        //bgImageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+
+        ordersView.makeCornerRadius(radius: 10)
+        worksView.makeCornerRadius(radius: 10)
+        aboutUsView.makeCornerRadius(radius: 10)
+        contactUsView.makeCornerRadius(radius: 10)
+
+
+        self.bgImageView.clipsToBounds = true
+
     }
+
 
 
 }
